8495cd0b63961e84213c9f47ed6b3e68cc606e19
unknown <anddxd@hotmail.com>
2021-03-26 08:56:02 -0400

    Adicao de scripts de criacao de banco, procedure e function

---
+++ src/BanksPortifolio.Business/SQLScripts/DatabaseCreationScript.sql
@@ -0,0 +1,41 @@
+﻿create database banksportfolio
+
+use banksportfolio
+
+create table ClientSector(
+	Id int IDENTITY(1,1) PRIMARY KEY,
+	Name varchar(60) NOT NULL
+)
+
+insert into ClientSector (Name) Values ('PUBLIC')
+insert into ClientSector (Name) Values ('PRIVATE')
+
+select * from ClientSector
+
+create table CheckedTrade(
+	Id int IDENTITY(1,1) PRIMARY KEY,
+	Name varchar(60) NOT NULL,
+	Value float NOT NULL,
+	ClientSectorId int FOREIGN KEY REFERENCES ClientSector(Id)
+)
+
+select * from CheckedTrade
+
+create table TradeCategory(
+	Id int IDENTITY(1,1) PRIMARY KEY,
+	Name varchar(60) NOT NULL
+)
+
+insert into TradeCategory (Name) Values ('LOWRISK')
+insert into TradeCategory (Name) Values ('MEDIUMRISK')
+insert into TradeCategory (Name) Values ('HIGHRISK')
+
+select * from TradeCategory
+
+create table CheckedTradeResult(
+	Id int IDENTITY(1,1) PRIMARY KEY,	
+	CheckedTradeId int FOREIGN KEY REFERENCES CheckedTrade(Id),
+	TradeCategoryId int FOREIGN KEY REFERENCES TradeCategory(Id)
+)
+
+select * from CheckedTradeResult
---
+++ src/BanksPortifolio.Business/SQLScripts/ProcedureAndFunctionCreationScript.sql
@@ -0,0 +1,60 @@
+﻿use banksportfolio
+
+create or alter function fn_GetTradeCategory(@tradeValue float, @tradeClientSector int)
+returns int
+as
+begin
+
+declare @TradeValueRange float
+select @TradeValueRange = 1000000
+
+declare @result int
+	if Exists(select Id from ClientSector where Id = @tradeClientSector)
+	begin
+		if(@tradeValue <= @TradeValueRange and @tradeClientSector = 1)
+		select @result = 1
+
+		if(@tradeValue > @TradeValueRange and @tradeClientSector = 1)
+		select @result = 2
+
+		if(@tradeValue > @TradeValueRange and @tradeClientSector = 2)
+		select @result = 3
+	end
+
+	else
+	begin
+		select @result = cast('Client Sector not found' as int)
+	end
+
+	return @result
+	
+end
+
+create or alter PROCEDURE sp_PersistInformationFromCheckedTrades
+@tradeName varchar(60),
+@tradeValue float,
+@tradeClientSector int
+as
+if Exists(select Id from ClientSector where Id = @tradeClientSector)
+insert into CheckedTrade (Name, Value, ClientSectorId) Values (@tradeName, @tradeValue, @tradeClientSector)
+
+declare @temp int
+select @temp = SCOPE_IDENTITY()
+
+declare @tradeCategoryId int
+select @tradeCategoryId = dbo.fn_GetTradeCategory(@tradeValue, @tradeClientSector)
+
+insert into CheckedTradeResult (CheckedTradeId, TradeCategoryId) Values (@temp, @tradeCategoryId)
+
+
+--USAGE
+--exec sp_PersistInformationFromCheckedTrades @tradeName = 'Trade1', @tradeValue = 2000000, @tradeClientSector = 2
+--exec sp_PersistInformationFromCheckedTrades @tradeName = 'Trade2', @tradeValue = 400000, @tradeClientSector = 1
+--exec sp_PersistInformationFromCheckedTrades @tradeName = 'Trade3', @tradeValue = 400000, @tradeClientSector = 1
+--exec sp_PersistInformationFromCheckedTrades @tradeName = 'Trade4', @tradeValue = 3000000, @tradeClientSector = 1
+
+--CHECK
+--select ct.Name, td.Name from CheckedTradeResult ctr
+--inner join CheckedTrade ct ON ct.Id = ctr.CheckedTradeId
+--inner join TradeCategory td ON td.Id = ctr.TradeCategoryId
+
